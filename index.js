a22fa18b0bd49746f6871e3ac5efce2801f5050f
brian <briangonzalez.9406@gmail.com>
2025-12-03 16:14:41 -0500

    app

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3301;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.post("/data", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.status(200).send("Data received");
+});
